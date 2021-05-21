42c5911dbda7a43c1c567e8706029d709dedf8b7
Derek Kershner <dkershner@cbtnuggets.com>
2021-05-21 15:17:43 -0700

    Simple POST api call action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { getInput, info, setFailed } from '@actions/core';
+import axios from 'axios';
+
+const parseJsonSafely = (jsonString: string): any => {
+    try {
+        return JSON.parse(jsonString);
+    } catch (error) {
+        return {};
+    }
+};
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+    try {
+        const url: string = getInput('url');
+        info('Sending POST request...');
+        const data = parseJsonSafely(getInput('data'));
+        const headers = parseJsonSafely(getInput('headers'));
+        await axios.post(url, data, {
+            headers,
+        });
+    } catch (error) {
+        setFailed(error.message);
+    }
 }
 
-run()
+run();
